91687091fb1073edc68541e005db5ba71278729d
Volodymyr Bondariev <vladmikhbond@hotmail.com>
2017-05-09 16:34:24 +0300

    enum Oblast

--- Sample/Form1.Designer.cs
+++ Sample/Form1.Designer.cs
@@ -29,35 +29,46 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pBox
+            // panel1
             // 
-            this.pBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(894, 555);
+            this.panel1.TabIndex = 0;
+            // 
+            // pBox
+            // 
             this.pBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pBox.Image = ((System.Drawing.Image)(resources.GetObject("pBox.Image")));
             this.pBox.InitialImage = null;
-            this.pBox.Location = new System.Drawing.Point(15, 14);
-            this.pBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pBox.Location = new System.Drawing.Point(0, 0);
+            this.pBox.Margin = new System.Windows.Forms.Padding(2);
             this.pBox.Name = "pBox";
-            this.pBox.Size = new System.Drawing.Size(1206, 670);
-            this.pBox.TabIndex = 1;
+            this.pBox.Size = new System.Drawing.Size(858, 513);
+            this.pBox.TabIndex = 3;
             this.pBox.TabStop = false;
             this.pBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseMove);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1235, 697);
-            this.Controls.Add(this.pBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(1139, 602);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Sample";
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
             this.ResumeLayout(false);
 
@@ -65,6 +76,7 @@
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pBox;
     }
 }
